d8d663f33f37662cb6f536c4cce68b583e77a843
Alvin Chen <alvinchen1988@icloud.com>
2016-03-07 14:23:42 -0800

    add main menu

---
+++ contact_list.rb
@@ -0,0 +1,21 @@
+require_relative 'contact'
+
+# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
+class ContactList
+
+  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+
+end
+
+# first, second, third = ARGV 
+# puts "The script is called:"
+# puts "Your first variable is: #{first}"
+# puts "Your second variable is: #{second}"
+# puts "Your third variable is: #{third}"
+
+puts "Here is a list of available commands: "
+puts "     new      - Create a new contact"
+puts "     list     - List all contacts"
+puts "     show     - Show a contact"
+puts "     search   - Search contacts"
+
