ba759125f1b9143a7d1be13e8d580dbe22355c52
Alvin Chen <alvinchen1988@icloud.com>
2016-03-10 04:55:49 +0000

    add all features except shell script

--- contact.rb
+++ contact.rb
@@ -4,6 +4,11 @@ require 'csv'
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
+  @@contact_content = CSV.read('seed.csv')
+
+  # @@contact_contents_hash = 
+  # [[1,"alvin","alin@icloud.com"], [1,"tina", "tina@hotmail.com"]]
+
   attr_accessor :name, :email
   
   # Creates a new contact object
@@ -11,15 +16,28 @@ class Contact
   # @param email [String] The contact's email address
   def initialize(name, email)
     # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
   end
+
+
 
   # Provides functionality for managing contacts in the csv file.
   class << self
 
+    def convert_csv_to_array_hash
+      contacts = []
+        @@contact_content.each do |inner_array|
+          contact = { id: inner_array[0],name: inner_array[1], email: inner_array[2]}
+          contacts << contact
+        end
+      contacts
+    end
+
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      self.convert_csv_to_array_hash
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
@@ -27,6 +45,29 @@ class Contact
     # @param email [String] the contact's email
     def create(name, email)
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+      contact = Contact.new(name, email)
+      if @@contact_content.empty?
+        id = 1
+      else
+        id = @@contact_content.last.first.to_i + 1
+      end
+
+      contacts = self.convert_csv_to_array_hash
+
+      contacts.each do |contact|
+        return false if contact[:email] == email
+      end
+
+      contact_array = []
+
+      contact_array << id
+      contact_array << contact.name
+      contact_array << contact.email
+
+      CSV.open('seed.csv', 'ab') do |csv_object|
+        csv_object << contact_array
+      end
+      contact = {name: contact.name, id: id}
     end
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
@@ -34,14 +75,26 @@ class Contact
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      id = id - 1
+      contact = @@contact_content[id]
     end
     
     # Search for contacts by either name or email.
     # @param term [String] the name fragment or email fragment to search for
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
+
+      term = term.downcase
+      contacts = self.convert_csv_to_array_hash
+
+      fillter_contacts = contacts.select do |contact|
+        contact[:name].downcase.include?(term) or contact[:email].downcase.include?(term)
+      end
+
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
     end
+
+
 
   end
 
--- contact_list.rb
+++ contact_list.rb
@@ -2,20 +2,161 @@ require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
+  attr_accessor :user_input, :first_param, :second_param, :param_length
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  def initialize
+    @user_input = ARGV
+    @first_param = @user_input[0]
+    @second_param = @user_input[1]
+    @param_length = @user_input.length
+  end
+
+  def welcome_screen
+    output = "Here is a list of available commands for browsing through your contact book: \n"
+    output += "     new      - Create a new contact\n"
+    output += "     list     - List all contacts\n"
+    output += "     show     - Show a contact\n"
+    output += "     search   - Search contacts"
+  end
+
+  def index
+    contacts = Contact.all
+    # return "The Contact book is empty" if contacts.empty?
+    contact_output = ""
+    num_of_records = contacts.length
+    contacts.each do |contact|
+      contact_output += "#{contact[:id]}: #{contact[:name]} (#{contact[:email]})\n"
+    end
+      contact_output += "---\n#{num_of_records} records total"
+  end
+
+  def new_contact(name, email)
+    contact= Contact.create(name,email)
+    puts contact ? "#{contact[:name]} of id:#{contact[:id]} is succesfully added to the contact list" : "#{email} already exist in your contact book" 
+  end
+
+  def find_contact(id)
+    contact = Contact.find(id)
+    # puts 1
+    puts contact.nil? ? "record with id:#{id} not found" : "#{contact[1]}\n#{contact.last}"
+  end
+
+  def search_contact(term)
+    filtered_contacts = Contact.search(term)
+    # contact_info = contacts[:contacts]
+    # contact_number = contacts[:num_of_records]
+
+    return puts "Not found" if filtered_contacts.empty?
+
+    filtered_contacts.each do |contact|
+      puts "#{contact[:id]} #{contact[:name]} (#{contact[:email]})" #contact[1] is the index position in the contact array which contains the user id
+    end
+
+    puts "------\n#{filtered_contacts.length} records found"
+
+  end
 
 end
 
-# first, second, third = ARGV 
-# puts "The script is called:"
-# puts "Your first variable is: #{first}"
-# puts "Your second variable is: #{second}"
-# puts "Your third variable is: #{third}"
+contact_list = ContactList.new
 
-puts "Here is a list of available commands: "
-puts "     new      - Create a new contact"
-puts "     list     - List all contacts"
-puts "     show     - Show a contact"
-puts "     search   - Search contacts"
+if contact_list.param_length == 0
+  puts contact_list.welcome_screen
+end
+
+case contact_list.first_param
+
+  when "list" 
+    contacts = contact_list.index.split("\n")
+    contacts_rows_displayed = contacts.length
+    begin
+      puts "Press enter key to see the next 5 contacts" if contacts_rows_displayed > 5
+      total_record_string = contacts.pop
+      pagination = contacts.shift(5)
+      pagination = pagination.join("\n").chomp("\n")
+      print pagination
+      contacts_rows_displayed -= 5
+    end
+    while(contacts_rows_displayed > 0)
+      user_input = STDIN.gets.chomp
+      if user_input == ""
+        pagination = contacts.shift(5)
+        pagination = pagination.join("\n").chomp("\n")
+        print pagination
+        contacts_rows_displayed -= 5
+      end
+      # puts "hello"
+    end
+    puts "\n#{total_record_string}"
+
+  when "new"
+    print "Enter the name of the new contact: "
+    name = STDIN.gets.chomp
+    print "Enter the email of the new contact: "
+    email = STDIN.gets.chomp
+    contact_list.new_contact(name, email)
+
+  when "show" 
+    if contact_id = contact_list.second_param
+      contact_id = contact_id.to_i
+      contact_list.find_contact(contact_id)
+    else
+      puts "missing user id as the second argument of show"
+    end
+
+  when "search"
+    if contact_term = contact_list.second_param
+      contact_list.search_contact(contact_term)
+    else
+      puts "missing search keyword as the second argument of search"
+    end
+    
+end
+
+# if contact_list.first_param == "list"
+  # contacts = contact_list.index.split("\n")
+  # contacts_rows_displayed = contacts.length
+  # begin
+  #   puts "Press enter key to see the next 5 contacts"
+  #   pagination = contacts.shift(5)
+  #   pagination = pagination.join("\n").chomp("\n")
+  #   print pagination
+  # end
+  # while(contacts_rows_displayed > 0)
+  #   user_input = STDIN.gets.chomp
+  #   if user_input == ""
+  #     pagination = contacts.shift(5)
+  #     pagination = pagination.join("\n").chomp("\n")
+  #     print pagination
+  #     contacts_rows_displayed -= 5
+  #   end
+  #   # puts "hello"
+  # end
+# end
+
+# if contact_list.first_param == "new"
+#   print "Enter the name of the new contact: "
+#   name = STDIN.gets.chomp
+#   print "Enter the email of the new contact : "
+#   email = STDIN.gets.chomp
+#   contact_list.new_contact(name, email)
+# end
+
+# if contact_list.first_param == "show" 
+#   if contact_id = contact_list.second_param
+#     contact_id = contact_id.to_i
+#     contact_list.find_contact(contact_id)
+#   else
+#     puts "missing user id as the second argument of show"
+#   end
+# end
+
+# if contact_list.first_param == "search"
+#   if contact_term = contact_list.second_param
+#     contact_list.search_contact(contact_term)
+#   else
+#     puts "missing search keyword as the second argument of search"
+#   end
+# end
+
 
